1b75112fc328d17dedf87f729b5c43130dfecf3e
Tonyblair Mageto <127820412+TonyblairMageto@users.noreply.github.com>
2023-03-22 08:27:40 +0300

    Add files via upload

---
+++ calc2.py
@@ -0,0 +1,16 @@
+# To prompt the user to enter the first number
+number1 = input("Enter First Number:")
+
+# To prompt the user to enter the second number
+number2 = input("Enter second Number:")
+
+# Strings to Float
+number1 = float(number1)
+number2 = float(number2)
+
+# calculation
+sum = number1 * number2
+
+
+#print the result
+print("Sum of ", number1, "and", number2, "is", sum)
